7e11c64652b3ccb7862dabd3b727a6081a6a838c
Schokobonlean <s212529@student.dhbw-mannheim.de>
2024-03-08 22:00:05 +0100

    created database

---
+++ create_database.py
@@ -0,0 +1,20 @@
+def create_database():
+    with open("mpls\mpls_home.txt", "r") as file:
+        lines = file.readlines()
+        with open("database.txt", "a") as database_file:
+            for line in lines:
+                print(line.strip())
+                for i in range(1,6):
+                    print(i)
+                    advice = input().replace("\n", " ")
+                    result = "### Question: Give me Advice on the folling mpl error: " + line.strip() + "### Answer: " + advice + "\n"
+                    database_file.write(result)
+
+def fix_questions():
+    with open("database.txt", "r") as file:
+        lines = file.readlines()
+        with open("database_fixed.txt", "w") as database_file:
+            for line in lines:
+                    database_file.write(line.replace("### Question: ", "### Question: Give me Advice on the folling mpl error: "))
+
+create_database()
--- mpl.py
+++ mpl.py
@@ -1,6 +1,7 @@
 import requests
 import xml.etree.ElementTree as ET
 import secret_file
+import re
 
 class Entry:
     def __init__(self, message_guid = None, iflow_name = None, error_text = None, ki_response = None, in_charge = None):
@@ -10,15 +11,18 @@ class Entry:
         self.ki_response = ki_response
         self.in_charge = in_charge
 
-beginning_period = "2024-02-01T00:00:00"
-end_period = "2024-02-01T00:05:00"
-host = "https://cbsintegration.it-cpi001.cfapps.eu10.hana.ondemand.com"
-username = secret_file.username
-password = secret_file.password
+beginning_period = "2024-03-01T00:00:10"
+end_period = "2024-03-01T00:00:05"
+home_host = "https://cbsintegration.it-cpi001.cfapps.eu10.hana.ondemand.com"
+client_host = "https://l100956-tmn.hci.eu1.hana.ondemand.com"
+client_host_prod = "https://l100957-tmn.hci.eu1.hana.ondemand.com"
+host = client_host_prod
+username = secret_file.client_username
+password = secret_file.client_password
 
 def get_mpls():
 
-    mpl_url = f"{host}/api/v1/MessageProcessingLogs?$inlinecount=allpages&$filter=Status eq 'FAILED' and LogStart gt datetime'{beginning_period}' and LogEnd lt datetime'{end_period}'&$select=MessageGuid,IntegrationFlowName"
+    mpl_url = f"{host}/api/v1/MessageProcessingLogs?$inlinecount=allpages&$filter=Status eq 'FAILED'"# and LogStart gt datetime'{beginning_period}' and LogEnd lt datetime'{end_period}'&$select=MessageGuid,IntegrationFlowName"
     session = requests.Session()
 
     try:
@@ -34,7 +38,6 @@ def get_mpls():
         message_guid = entry_elem.find(".//{http://schemas.microsoft.com/ado/2007/08/dataservices}MessageGuid").text
         iflow_name = entry_elem.find(".//{http://schemas.microsoft.com/ado/2007/08/dataservices}IntegrationFlowName").text
         entries.append(Entry(message_guid, iflow_name))
-
     for entry in entries:
         error_url = f"{host}/api/v1/MessageProcessingLogs('{entry.message_guid}')/ErrorInformation/$value"
         try:
@@ -43,5 +46,19 @@ def get_mpls():
             entry.error_text = response.text
         except requests.exceptions.RequestException as e:
             raise e
-
     return entries
+
+def write_to_file(entries):
+    mesages = set()
+    for entry in entries:
+        mpl_id_pattern = r'The MPL ID for the failed message is : .+'
+        inner_error_pattern = r',? *("innerError":{.*?})'
+        message = re.sub(mpl_id_pattern, '', entry.error_text)
+        message = re.sub(inner_error_pattern, '', message)
+        mesages.add(message.replace("\n", ""))
+    with open("mpls_cient_prod.txt", "w") as file:
+        for message in mesages: 
+            file.write(message)
+            file.write("\n")
+
+write_to_file((get_mpls()))
